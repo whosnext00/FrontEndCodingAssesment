0e5af77867352a1d872cc679c6f560fc3ccce638
whosnext00 <contact@justinhubbard.me>
2014-05-06 17:23:05 -0400

    Task F1
    Completed this task

---
+++ Task F1/script.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function($) {
+	function handleFileSelect(evt) {
+		var files = evt.target.files;
+		f = files[0];
+		var reader = new FileReader();
+			reader.onload = (function(theFile) {
+			return function(e) {
+				var span = document.createElement('span');
+				span.innerHTML = ['<img class="thumb" src="', e.target.result,'" title="', escape(theFile.name), '"/>'].join('');
+				document.getElementById('img_placeholder').insertBefore(span, null);
+			};
+		})(f);
+		reader.readAsDataURL(f);
+	}
+	document.getElementById('my_img_upload_input').addEventListener('change', handleFileSelect, false);
+});
